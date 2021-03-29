e0b614f60ce6ce596c06cf639f62ef8955c92fcf
Meeky98 <69221732+Meeky98@users.noreply.github.com>
2021-03-29 17:45:45 -0400

    Add files via upload

---
+++ Checkout.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CST4708_Project;
+using CST4708_Project.TableClass;
+
+namespace CST_Project_Checkout_Page
+{
+    public partial class Checkout : Form
+    {
+        ArrayList orderId;
+
+        public Checkout(ArrayList orderId, double total, String userId)
+        {
+            InitializeComponent();
+            userID = userId;
+
+            this.orderId = orderId;
+            label14.Text = total.ToString();
+            
+        }
+        string userID;
+        DataTable mydt;
+        SqlConnection myconn;
+        SqlCommand mycmd;
+        SqlDataAdapter myadapter;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+             mydt = new DataTable();
+
+            
+            
+            myconn = new SqlConnection();
+            //myconn.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Dariel Meeks\\Downloads\\Project(1).mdf Integrated Security=True;Connect Timeout=30";
+            myconn.ConnectionString = UserTable.getAutoConnString();
+                     
+            myconn.Open();
+            mycmd = new SqlCommand();
+            mycmd.Connection = myconn;
+            Random rnd = new Random();
+            int order = rnd.Next(10000);
+
+            if (String.IsNullOrWhiteSpace(textBox1)(textBox2)(textBox3)(textBox4)(textBox5)(textBox6)(textBox7))
+                {
+                    MessageBox.Show("Please enter all information");
+                }
+            else
+                {
+                     mydt = new DataTable();
+
+                     myadapter = new SqlDataAdapter();
+                     myadapter.SelectCommand = mycmd;
+                  
+                        for (int i = 0; i < orderId.Count; i++) {
+                                mycmd.CommandText = "UPDATE Order_T " +
+                                "SET is_purchased ='Yes' " +
+                                "where order_id=" + orderId[i].ToString();
+                          
+                     MessageBox.Show(orderId[i].ToString());
+                     mycmd.ExecuteNonQuery();
+
+                     myadapter.UpdateCommand = mycmd;
+                     mycmd.CommandText = "UPDATE Product_T " +
+                                "SET on_hand = on_hand - 1 " +
+                                "Where product_id=" + product_id[i].ToInt();
+                    mycmd.ExecuteNonQuery();
+
+                        }
+                    
+
+              
+            MessageBox.Show("Thank you "+userID+" for your purchase. Your order number is #"+order+".");
+            this.Hide();
+            Main_menu m1 = new Main_menu(userID);
+            m1.ShowDialog();
+                }
+          
+            this.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            
+        }
+    }
+}
+
---
+++ Create_Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CST4708_Project.TableClass;
+
+namespace CST4708_Project
+{
+    public partial class Create_Account : Form
+    {
+        public Create_Account()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhitespace(tb_email)(tb_fName)(tb_lName)(tb_Login)(tb_password))
+                {
+                    MessageBox.Show("Please fill in all the information");
+                }
+            else
+                {
+                    UserTable.rowInsert(tb_Login.Text, tb_password.Text, tb_fName.Text, tb_lName.Text, tb_email.Text);
+                    MessageBox.Show("Account Created");
+                }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login_page r1 = new Login_page();
+            r1.ShowDialog();
+            this.Close();
+        }
+    }
+
+}
